1e052d363507cfc5504dc8cda37ec82acbeeefca
서정아 <48680894+jungaSeo@users.noreply.github.com>
2019-09-01 22:08:08 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 
 # Password validation
